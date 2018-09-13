7c6088ca130a7f12e6f23fe5f0680bd9f71c04c8
Robbie Martinus <robbie.martinus@mastercard.com>
2018-09-14 08:54:21 +1000

    Added more feature detections

--- src/main/java/gcp/VisionController.java
+++ src/main/java/gcp/VisionController.java
@@ -9,8 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-import static com.google.cloud.vision.v1.Feature.Type.DOCUMENT_TEXT_DETECTION;
-import static com.google.cloud.vision.v1.Feature.Type.LABEL_DETECTION;
+import static com.google.cloud.vision.v1.Feature.Type.*;
 import static java.util.Collections.singletonList;
 
 @RestController
@@ -25,7 +24,11 @@ public class VisionController {
     public String analyse(@RequestParam MultipartFile image) throws IOException {
         BatchAnnotateImagesResponse batchAnnotateImagesResponse = imageAnnotatorClient.batchAnnotateImages(
                 singletonList(AnnotateImageRequest.newBuilder()
+                        .addFeatures(Feature.newBuilder().setType(FACE_DETECTION).build())
+                        .addFeatures(Feature.newBuilder().setType(LANDMARK_DETECTION).build())
+                        .addFeatures(Feature.newBuilder().setType(LOGO_DETECTION).build())
                         .addFeatures(Feature.newBuilder().setType(LABEL_DETECTION).build())
+                        .addFeatures(Feature.newBuilder().setType(TEXT_DETECTION).build())
                         .addFeatures(Feature.newBuilder().setType(DOCUMENT_TEXT_DETECTION).build())
                         .setImage(Image.newBuilder()
                                 .setContent(ByteString.copyFrom(image.getBytes()))
