cc37e84168ea5603aa43abc02b7308156399cd45
Robbie Martinus <robbie.martinus@mastercard.com>
2018-09-14 11:28:56 +1000

    Added Controller test

---
+++ src/test/java/image/analyser/VisionControllerTest.java
@@ -0,0 +1,35 @@
+package image.analyser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class VisionControllerTest {
+    @Mock
+    private VisionService visionService;
+
+    @InjectMocks
+    private VisionController visionController;
+
+    @Test
+    public void shouldReturnAnalysisSuccessfullyWhenFileIsUploaded() throws IOException {
+        MultipartFile image = mock(MultipartFile.class);
+        byte[] imageBytes = "golden retriever".getBytes();
+        given(image.getBytes()).willReturn(imageBytes);
+        given(visionService.analyse(imageBytes)).willReturn("Dog, mammal, golden retriever");
+
+        String analysis = visionController.analyse(image);
+
+        assertThat(analysis).isEqualTo("Dog, mammal, golden retriever");
+    }
+}
